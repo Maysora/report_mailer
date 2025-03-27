6ff7ba14e7786a66998cacf29e2dac622daac7e2
Roy <dev@maysora.com>
2025-03-27 17:02:44 +0700

    minor UI updates:
    - summary rounded to 0 decimal points
    - add task weight percentage on wgs html mail template
    - add background to date if date equal to today

--- app/helpers/report_mails_helper.rb
+++ app/helpers/report_mails_helper.rb
@@ -1,4 +1,14 @@
 module ReportMailsHelper
+  def date_css_class(report_mail)
+    if report_mail.date == Time.zone.today
+      'text-bg-success d-inline-block px-2 rounded'
+    elsif !report_mail.sent?
+      'text-bg-warning d-inline-block px-2 rounded'
+    else
+      nil
+    end
+  end
+
   def task_stats(project)
     project.active_tasks.group_by(&:category).map do |category, tasks_by_category|
       "#{category.presence || 'N/A'} [" + [
